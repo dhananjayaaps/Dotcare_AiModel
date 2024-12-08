4a66200715d317f543f9069dcecdffea1c2d4ae1
dhananjayaaps <dhananjayaaps@gmail.com>
2024-12-08 20:37:43 +0530

    initial commit

---
+++ AI_vision.py
@@ -0,0 +1,37 @@
+import asyncio
+
+import httpx
+import os
+import base64
+import google.generativeai as genai
+from flask import jsonify
+from pinecone import Pinecone
+
+from credentials import GOOGLE_API_KEY
+
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel(model_name="gemini-1.5-pro")
+
+def askAboutMother(question):
+
+    prompt = f"""{question}"""
+    print(prompt)
+
+    print(prompt)
+    generation_config = {
+      "temperature": 1,
+      "top_p": 0.95,
+      "top_k": 40,
+      "max_output_tokens": 8192,
+      "response_mime_type": "text/plain",
+    }
+
+    model = genai.GenerativeModel(
+      model_name="gemini-1.5-flash",
+      generation_config=generation_config,
+    )
+    response = model.generate_content(prompt)
+
+    print(response.text)
+
+    return jsonify(response.text.split('|')), 200
---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify, Response
+from AI_vision import askAboutMother
+
+app = Flask(__name__)
+
+@app.route('/askAboutMother', methods=['POST'])
+def ask_about_model():
+    try:
+        data = request.get_json()
+        print(f"Received data: {data}")
+    except Exception as e:
+        print(f"Error parsing JSON: {e}")
+        return jsonify({"success": False, "message": str(e), "data": None}), 400
+
+    if not data or not all(key in data for key in ['query']):
+        return jsonify({"error": "Missing required fields"}), 400
+
+    question = data['query']
+    return askAboutMother(question)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
